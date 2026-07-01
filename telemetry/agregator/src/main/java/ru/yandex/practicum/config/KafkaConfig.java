243c3f4cb9a8cf2fbdd606fb61215e4969247918
Alexander Scatov <scatovalexander@yandex.ru>
2026-07-01 09:40:12 +0300

    fix producer config

--- telemetry/agregator/src/main/java/ru/yandex/practicum/config/KafkaConfig.java
+++ telemetry/agregator/src/main/java/ru/yandex/practicum/config/KafkaConfig.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import ru.yandex.practicum.kafka.telemetry.event.SensorEventAvro;
+import ru.yandex.practicum.kafka.telemetry.event.SensorsSnapshotAvro;
 
 import java.util.Collections;
 import java.util.Properties;
@@ -22,18 +23,16 @@ public class KafkaConfig {
     public KafkaConfig(KafkaProperties kafkaProperties) {
         this.kafkaProperties = kafkaProperties;
     }
+
     @Bean
     public KafkaConsumer<String, SensorEventAvro> kafkaConsumer() {
         Properties props = new Properties();
-
-        // Bootstrap servers (гарантированно строка)
         String bootstrapServers = kafkaProperties.getBootstrapServers();
         if (bootstrapServers == null || bootstrapServers.isBlank()) {
             bootstrapServers = "localhost:9092";
         }
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
 
-        // Group ID
         String groupId = kafkaProperties.getConsumer().getGroupId();
         if (groupId == null || groupId.isBlank()) {
             groupId = "smart-home-aggregator-group";
@@ -41,58 +40,40 @@ public class KafkaConfig {
         props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
 
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ru.yandex.practicum.config.SensorEventDeserializer.class.getName());
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, SensorEventDeserializer.class.getName());
 
-        // AUTO_OFFSET_RESET: строка
         String autoOffsetReset = kafkaProperties.getConsumer().getAutoOffsetReset();
         if (autoOffsetReset == null || autoOffsetReset.isBlank()) {
             autoOffsetReset = "earliest";
         }
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, autoOffsetReset);
 
-        // ENABLE_AUTO_COMMIT: boolean -> String
         boolean enableAutoCommit = kafkaProperties.getConsumer().isEnableAutoCommit();
         props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, String.valueOf(enableAutoCommit));
 
-        // MAX_POLL_INTERVAL_MS: int -> String
         Integer maxPollIntervalMs = kafkaProperties.getConsumer().getMaxPollIntervalMs();
-        if (maxPollIntervalMs == null) {
-            maxPollIntervalMs = 300000;
-        }
+        if (maxPollIntervalMs == null) maxPollIntervalMs = 300000;
         props.put(ConsumerConfig.MAX_POLL_INTERVAL_MS_CONFIG, String.valueOf(maxPollIntervalMs));
 
-        // SESSION_TIMEOUT_MS: int -> String
         Integer sessionTimeoutMs = kafkaProperties.getConsumer().getSessionTimeoutMs();
-        if (sessionTimeoutMs == null) {
-            sessionTimeoutMs = 10000;
-        }
+        if (sessionTimeoutMs == null) sessionTimeoutMs = 10000;
         props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, String.valueOf(sessionTimeoutMs));
 
-        // FETCH_MIN_BYTES: int -> String
-        Integer fetchMinBytes = kafkaProperties.getConsumer().getFetchMinBytes();
-        if (fetchMinBytes == null) {
-            fetchMinBytes = 1;
-        }
-        props.put(ConsumerConfig.FETCH_MIN_BYTES_CONFIG, String.valueOf(fetchMinBytes));
-
-        // FETCH_MAX_WAIT_MS: int -> String
-        Integer fetchMaxWaitMs = kafkaProperties.getConsumer().getFetchMaxWaitMs();
-        if (fetchMaxWaitMs == null) {
-            fetchMaxWaitMs = 500;
-        }
-        props.put(ConsumerConfig.FETCH_MAX_WAIT_MS_CONFIG, String.valueOf(fetchMaxWaitMs));
-
         KafkaConsumer<String, SensorEventAvro> consumer = new KafkaConsumer<>(props);
-        consumer.subscribe(Collections.singletonList(
-                kafkaProperties.getTopic().getSensorEvents() != null
-                        ? kafkaProperties.getTopic().getSensorEvents()
-                        : "telemetry.sensors.v1"
-        ));
+
+        // ПОДПИСЫВАЕМСЯ СРАЗУ ПРИ СОЗДАНИИ БИНА
+        String topicName = kafkaProperties.getTopic().getSensorEvents();
+        if (topicName == null || topicName.isBlank()) {
+            topicName = "telemetry.sensors.v1";
+        }
+        consumer.subscribe(Collections.singletonList(topicName));
+
         return consumer;
     }
 
+
     @Bean
-    public KafkaProducer<String, SpecificRecordBase> kafkaProducer() {
+    public KafkaProducer<String, SensorsSnapshotAvro> kafkaProducer() {
         Properties props = new Properties();
 
         String bootstrapServers = kafkaProperties.getBootstrapServers();
