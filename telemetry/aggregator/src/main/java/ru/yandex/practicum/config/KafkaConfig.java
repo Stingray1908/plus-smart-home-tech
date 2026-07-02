9de329e01a5bc2e5efe2b1b1efffd8e25d8dd494
Alexander Scatov <scatovalexander@yandex.ru>
2026-07-03 00:11:17 +0300

    рефакторинг config

--- telemetry/aggregator/src/main/java/ru/yandex/practicum/config/KafkaConfig.java
+++ telemetry/aggregator/src/main/java/ru/yandex/practicum/config/KafkaConfig.java
@@ -1,11 +1,10 @@
 package ru.yandex.practicum.config;
 
-import org.apache.avro.specific.SpecificRecordBase;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import ru.yandex.practicum.kafka.telemetry.event.SensorEventAvro;
@@ -53,13 +52,15 @@ public class KafkaConfig {
     @Bean
     public KafkaProducer<String, SensorsSnapshotAvro> kafkaProducer() {
         var props = new Properties();
-        props.put("bootstrap.servers", kafkaProperties.getBootstrapServers());
-        props.put("key.serializer", kafkaProperties.getProducer().getKeySerializer());
-        props.put("value.serializer", kafkaProperties.getProducer().getValueSerializer());
 
-        props.put("acks", kafkaProperties.getProducer().getAcks());
-        props.put("retries", String.valueOf(kafkaProperties.getProducer().getRetries()));
-        props.put("batch.size", String.valueOf(kafkaProperties.getProducer().getBatchSize()));
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaProperties.getBootstrapServers());
+
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, kafkaProperties.getProducer().getKeySerializer());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, kafkaProperties.getProducer().getValueSerializer());
+
+        props.put(ProducerConfig.ACKS_CONFIG, String.valueOf(kafkaProperties.getProducer().getAcks()));
+        props.put(ProducerConfig.RETRIES_CONFIG, String.valueOf(kafkaProperties.getProducer().getRetries()));
+        props.put(ProducerConfig.BATCH_SIZE_CONFIG, String.valueOf(kafkaProperties.getProducer().getBatchSize()));
 
         return new KafkaProducer<>(props);
     }
