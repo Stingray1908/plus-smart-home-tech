f912586769463fc3b2d5b81322b3ab7ee2ced056
Alexander Scatov <scatovalexander@yandex.ru>
2026-07-02 15:52:29 +0300

    настройки вынесены в отдельный класс анализатор

--- telemetry/analyzer/src/main/java/ru.yandex.practicum/Analyzer.java
+++ telemetry/analyzer/src/main/java/ru.yandex.practicum/Analyzer.java
@@ -5,12 +5,14 @@ import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ConfigurableApplicationContext;
 import ru.yandex.practicum.kafka.HubEventProcessor;
 import ru.yandex.practicum.kafka.SnapshotProcessor;
 
 @Slf4j
 @SpringBootApplication
+@EnableConfigurationProperties(ru.yandex.practicum.config.KafkaProperties.class)
 public class Analyzer {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(Analyzer.class, args);
