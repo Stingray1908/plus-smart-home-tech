f57b529c65a451aabbdaf58ec70a64c534933b00
Alexander Scatov <scatovalexander@yandex.ru>
2026-07-24 11:17:24 +0300

    fix: discovery-server/pom: исправлен parent и добавлен импорт

--- commerce/shopping-store/src/main/java/ru/yandex/practicum/controller/GlobalExceptionHandler.java
+++ commerce/shopping-store/src/main/java/ru/yandex/practicum/controller/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import ru.yandex.practicum.common.ErrorResponse;
 import ru.yandex.practicum.exception.ProductNotFoundException;
 
 import java.time.Instant;
--- commerce/warehouse/src/main/java/ru/yandex/practicum/controller/GlobalExceptionHandler.java
+++ commerce/warehouse/src/main/java/ru/yandex/practicum/controller/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.yandex.practicum.common.ErrorResponse;
 import ru.yandex.practicum.exception.ProductInShoppingCartLowQuantityInWarehouse;
 
 import java.time.Instant;
